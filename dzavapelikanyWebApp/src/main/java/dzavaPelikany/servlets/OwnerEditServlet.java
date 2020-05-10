109c498af35dc6fba9f5f061d782b39f030a19f5
Paweł <pawel.molesta@gmail.com>
2020-05-10 13:34:59 +0200

    pom.xml update

--- dzavapelikanyWebApp/src/main/java/dzavaPelikany/servlets/OwnerEditServlet.java
+++ dzavapelikanyWebApp/src/main/java/dzavaPelikany/servlets/OwnerEditServlet.java
@@ -1,5 +1,10 @@
 package dzavaPelikany.servlets;
 
+import dzavaPelikany.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,16 +12,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 
 @WebServlet("/owner-edit-details")
 public class OwnerEditServlet extends HttpServlet {
 
+    @Inject
+    private TemplateProvider templateProvider;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Template template = templateProvider.getTemplate(getServletContext(), "owner-edit-screen.ftlh");
+
+        response.setContentType("text/html;charset=UTF-8");
+
         PrintWriter printWriter = response.getWriter();
-        request.setCharacterEncoding("UTF-8");
-        request.getRequestDispatcher("owner-edit-screen.html").forward(request, response);
+        try {
+            template.process(new HashMap<String, Object>(), printWriter);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
 
     }
 }
